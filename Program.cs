8ec5fc071a7f4aded95fbc46f312abe0b20bc862
nicoli silva <nicoli_silva151@estudante.sesisenai.org.br>
2026-06-10 21:55:09 -0300

    Impar ou par

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Informe um numero>:!");
+int variavelQueSalvaUmNumero = int.Parse(Console.ReadLine()!);
+if (variavelQueSalvaUmNumero % 2 == 0) {
+    Console.WriteLine($"Numero {variavelQueSalvaUmNumero} e par");
+} else {
+    Console.WriteLine($"Numero {variavelQueSalvaUmNumero} e impar");
+}
+
+
